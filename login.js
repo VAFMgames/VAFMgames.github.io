ed2f36b231f199e0a048836f9e61b23febbce736
VAFMgames <hugo.valet@outlook.fr>
2025-11-25 19:03:24 +0100

    Update login.js

--- login.js
+++ login.js
@@ -4,14 +4,14 @@
     from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
   // Config Firebase (à récupérer dans ta console Firebase)
-  const firebaseConfig = {
-    apiKey: "TA_CLE_API",
-    authDomain: "ton-projet.firebaseapp.com",
-    projectId: "ton-projet",
-    storageBucket: "ton-projet.appspot.com",
-    messagingSenderId: "XXXXXXX",
-    appId: "XXXXXXX"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyA6S2lmIUwKUFptBrJKftVyP_F6j-X2XGM",
+  authDomain: "sign-up-vafm.firebaseapp.com",
+  projectId: "sign-up-vafm",
+  storageBucket: "sign-up-vafm.firebasestorage.app",
+  messagingSenderId: "561614233020",
+  appId: "1:561614233020:web:4a0c3658e343cb74271fb9"
+};
 
   // Initialisation
   const app = initializeApp(firebaseConfig);
